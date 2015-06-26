9c59593411eb5e596b775b980ee9122255fd4415
Fredric Moezinia <moezinia@mit.edu>
2015-06-26 12:18:25 -0700

    Update product_search.py

--- product_search.py
+++ product_search.py
@@ -1,5 +1,8 @@
 from amazon.api import AmazonAPI
 
+#WILL NOT WORK WITH OUT OS.ENVIRON CREDENTIALS FROM AWS AMAZON ETC
+
+
 class Itemsearch(object):
 	
 
@@ -9,10 +12,7 @@ class Itemsearch(object):
 	def prod_search(self):
 
 		#insert amazon web services credentials
-		AMAZON_ACCESS_KEY = 'AKIAJGEKFL3UEU6QMCPQ'
-		AMAZON_SECRET_KEY = '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'
 		#associate TAG must be updated every 180 days, make new amazon associates account to get new tag
-		AMAZON_ASSOC_TAG = 'ignacio0ba-20'
 
 		amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
 
